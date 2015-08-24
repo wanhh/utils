781b38384ecff9b1cd09a1f6822d9dc1af2e8f10
wanhh <279915303@qq.com>
2015-08-24 15:02:33 +0800

    Update tplEngine.js

--- tplEngine.js
+++ tplEngine.js
@@ -1,3 +1,8 @@
+/*
+* AbsurdJS 作者写的一篇教程，一步步教你怎样用 Javascript 实现一个纯客户端的模板引擎。
+* 整个引擎实现只有不到 20 行代码。
+* 但是这个代码有点小缺陷，就是不支持预编译，这里加了2行代码使它能够支持预编译；
+* */
 var utils=utils||{};
 utils.tplEngine = function(html, options) {
     var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
